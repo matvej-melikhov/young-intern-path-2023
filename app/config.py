ff0f4e9e9e8e05d1d30e1f6bd0217fd18574e6ab
Matvej Melikhov <matvej.melikhov@yandex.ru>
2023-08-04 16:45:33 +0300

    Harden config and templates

--- app/config.py
+++ app/config.py
@@ -1,12 +1,13 @@
 import datetime
+import os
 
 class Config(object):
-    SECRET_KEY = "A0Zr98j/3yX R~XHH!jmN]LWX/,?RT"
-    SQLALCHEMY_DATABASE_URI = "sqlite:///base.db"
+    SECRET_KEY = os.environ.get("SECRET_KEY", "dev-secret-key-change-me")
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URI", "sqlite:///base.db")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JSON_AS_ASCII = False
 
 QUESTIONS_AMOUNT = 30
 MAX_SCORE = 90
 GAME_START_TIME = datetime.datetime(2023, 9, 1, 9, 30, 00)
-ADMIN_CODE = "hamapa23"
+ADMIN_CODE = os.environ.get("ADMIN_CODE", "hamapa23")
--- main.py
+++ main.py
@@ -1,4 +1,9 @@
+import os
+
 from app import application
 
 if __name__ == "__main__":
-    application.run(debug=True)
+    debug = os.environ.get("FLASK_DEBUG", "0") == "1"
+    host = os.environ.get("HOST", "127.0.0.1")
+    port = int(os.environ.get("PORT", 5000))
+    application.run(debug=debug, host=host, port=port)
